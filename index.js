d5703e7776fbb7cb4b6d662135437f6fd0e08282
Hernando Bifaretti <hbifaretti@gmail.com>
2022-06-21 16:14:09 +0200

    done-whithoutBonus

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function (a, b) {
+      return a - b;
+    });
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (!this.length) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    if (!this.length) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.min(...this.items);
+  }
 
   sum() {}
 
